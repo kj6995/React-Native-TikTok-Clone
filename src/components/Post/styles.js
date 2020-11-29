fcf53fcd50cb2b5fc0861e10125cdcd4f7519378
kj6995 <kushagra.me14iitp@gmail.com>
2020-11-29 16:33:24 +0530

    Post component done

---
+++ src/components/Post/styles.js
@@ -0,0 +1,77 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const styles = StyleSheet.create({
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  container: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+  },
+  uiContainer: {
+    height: '100%',
+    justifyContent: 'flex-end',
+  },
+  rightContainer: {
+    alignSelf: 'flex-end',
+    height: 300,
+    justifyContent: 'space-between',
+    marginRight: 5,
+  },
+  profilePicture: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  iconContainer: {
+    alignItems: 'center',
+  },
+  statsLabel: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 5,
+  },
+  bottomContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  handle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+    marginBottom: 5,
+  },
+  description: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '300',
+    marginBottom: 10,
+  },
+  songRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  songName: {
+    color: 'white',
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  songImage: {
+    height: 35,
+    width: 35,
+    borderRadius: 25,
+    borderWidth: 8,
+    borderColor: '#4c4c4c',
+  },
+});
+
+export default styles;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import Post from '../../components/Post';
+
+const post1 = {
+  id: 'p1',
+  videoUri:
+    'https://biteable.com/static-assets/homepage/HubpageVideo_Instagram_16x9_01.mp4',
+  user: {
+    id: 'u1',
+    username: 'smartycat',
+    imageUri:
+      'https://images.unsplash.com/profile-1446404465118-3a53b909cc82?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=27a346c2362207494baa7b76f5d606e5',
+  },
+  description: "hahahaha that's good boy @borat",
+  songName: 'NF - The search',
+  songImage:
+    'https://images.unsplash.com/profile-1446404465118-3a53b909cc82?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=27a346c2362207494baa7b76f5d606e5',
+  likes: 168,
+  comments: 12,
+  shares: 44,
+};
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Post post={post1} />
     </View>
   );
 };
