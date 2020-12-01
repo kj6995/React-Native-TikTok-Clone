e50258e689a5e3f316f6e9cde62966f45ea15603
kj6995 <kushagra.me14iitp@gmail.com>
2020-12-01 14:02:42 +0530

    connected cognito user to database user

--- App.js
+++ App.js
@@ -6,13 +6,71 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
-import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
 import Navigation from './src/navigation';
 
+import {Auth, API, graphqlOperation} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 
+import {createUser} from './graphql/mutations';
+import {getUser} from './graphql/queries';
+
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)];
+};
+
 const App: () => React$Node = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        // get currently authenticated user from cognito service
+        const userInfo = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+
+        if (!userInfo) {
+          return;
+        }
+        // check if tHE user exist in database
+        const getUserResponse = await API.graphql(
+          graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+        );
+
+        if (getUserResponse.data.getUser) {
+          console.log('User already exists in database');
+          return;
+        }
+        // if it doesn't exist
+        // then, create a new user in database
+        const newUser = {
+          id: userInfo.attributes.sub,
+          username: userInfo.username,
+          email: userInfo.attributes.email,
+          imageUri: getRandomImage(),
+        };
+
+        await API.graphql(
+          graphqlOperation(createUser, {
+            input: newUser,
+          }),
+        );
+      } catch (e) {
+        console.log('Error Error');
+        console.error(e);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" />
@@ -22,7 +80,5 @@ const App: () => React$Node = () => {
     </>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default withAuthenticator(App);
